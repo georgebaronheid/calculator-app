2ed0ea14c72aa95ada1140b9f2f46e80a3a454ee
George Baronheid <baronheid.george@gmail.com>
2020-07-08 13:36:49 -0300

    Refactor

--- app/src/main/java/br/com/baronheid/calculatorlesson/MainActivity.kt
+++ app/src/main/java/br/com/baronheid/calculatorlesson/MainActivity.kt
@@ -6,10 +6,10 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var resultEditText: EditText
-    private lateinit var numberInputEditText: EditText
     private val operationTextView
             by lazy(LazyThreadSafetyMode.NONE) { findViewById<TextView>(R.id.operation_main_activity) }
 
@@ -21,74 +21,51 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        resultEditText = findViewById(R.id.result_main_activity)
-        numberInputEditText = findViewById(R.id.number_input_main_activity)
 
-        val numberButtonArray = arrayOfNumberButtons()
+        val numberButtonsArray = arrayOfNumberButtons()
+        val operationButtonsArray = arrayOfOperationButtons()
 
-
-        //        Operation Buttons
-        val buttonEquals = findViewById<Button>(R.id.button_equals)
-        val buttonPlus = findViewById<Button>(R.id.button_plus)
-        val buttonMinus = findViewById<Button>(R.id.button_minus)
-        val buttonDivide = findViewById<Button>(R.id.button_divide)
-        val buttonMultiply = findViewById<Button>(R.id.button_multiply)
-        val operationsList =
-            arrayOf(buttonEquals, buttonPlus, buttonMinus, buttonDivide, buttonMultiply)
-
-
-        val listener = View.OnClickListener { v ->
+        val buttonInputListener = View.OnClickListener { v ->
             val b = v as Button
-            numberInputEditText.append(b.text)
+            number_input_main_activity.append(b.text)
         }
 
-        numberButtonArray.forEach {
-            it.setOnClickListener(listener)
+        numberButtonsArray.forEach {
+            it.setOnClickListener(buttonInputListener)
         }
 
         val operationListener = View.OnClickListener { v ->
             val operation = (v as Button).text.toString()
-            val value = numberInputEditText.text.toString()
+            val value = number_input_main_activity.text.toString()
 
             if (value.isNotEmpty()) performOperation(value, operation)
 
             pendingOperation = operation
             operationTextView.text = pendingOperation
         }
-        operationsList.forEach {
+        operationButtonsArray.forEach {
             it.setOnClickListener(operationListener)
         }
-
-
     }
 
     private fun arrayOfNumberButtons(): Array<Button> {
-        //        Data input buttons
-        val buttonZero: Button = findViewById(R.id.button_0)
-        val buttonOne: Button = findViewById(R.id.button_1)
-        val buttonTwo: Button = findViewById(R.id.button_2)
-        val buttonThree: Button = findViewById(R.id.button_3)
-        val buttonFour: Button = findViewById(R.id.button_4)
-        val buttonFive: Button = findViewById(R.id.button_5)
-        val buttonSix: Button = findViewById(R.id.button_6)
-        val buttonSeven: Button = findViewById(R.id.button_7)
-        val buttonEight: Button = findViewById(R.id.button_8)
-        val buttonNine: Button = findViewById(R.id.button_9)
-        val buttonDot: Button = findViewById(R.id.button_dot)
-
         return arrayOf(
-            buttonZero,
-            buttonOne,
-            buttonTwo,
-            buttonThree,
-            buttonFour,
-            buttonFive,
-            buttonSix,
-            buttonSeven,
-            buttonEight,
-            buttonNine,
-            buttonDot
+            button_0,
+            button_1,
+            button_2,
+            button_3,
+            button_4,
+            button_5,
+            button_6,
+            button_7,
+            button_8,
+            button_9,
+            button_dot
         )
+    }
+
+    private fun arrayOfOperationButtons(): Array<Button> {
+        return arrayOf(button_equals, button_plus, button_minus, button_divide, button_multiply)
     }
 
     private fun performOperation(value: String, operation: String) {
